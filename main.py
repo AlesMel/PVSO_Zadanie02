63900b877127c0aaf65ab275e32b126cb0934869
Aleš Melichar <alesmelichar13@gmail.com>
2023-03-01 15:09:13 +0100

    the main method was initialised incorrectly

--- main.py
+++ main.py
@@ -1,32 +1,26 @@
-from time import sleep
-from ximea import xiapi
 import cv2 as cv
 import numpy as np
 
+WIDTH: int = 400
+HEIGHT: int = 400
+def process_image(width, height):
+    ret, img = cam.read()
+    image = cv.resize(img, (width, height))
+    return image
 
 def setup_camera():
-    cam = xiapi.Camera()
-    # settings
-    cam.open_device()
-    cam.set_exposure(1e5)
-    cam.set_param("imgdataformat", "XI_RGB32")
-    cam.set_param("auto_wb", 1)
-    cam.start_acquisition()
+    cam = cv.VideoCapture(0)
     return cam
-
-
-def process_image(width, height):
-    cam.get_image(img)
-    image = img.get_image_data_numpy()
-    image = cv.resize(image, (width, height))
-    return image
 
 cam = setup_camera()
 
-while True:
-    processed_image = process_image(400, 300)
-    if cv.waitKey(1) == ord('q'):
-        break
+while cv.waitKey(1) != ord('q'):
+    processed_image = process_image(WIDTH, HEIGHT)
+    cv.imshow("img", processed_image)
 
-cam.stop_acquisition()
-cam.close_device()
+
+
+# After the loop release the cap object
+cam.release()
+# Destroy all the windows
+cv.destroyAllWindows()
