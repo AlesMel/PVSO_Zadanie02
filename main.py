4fb7e0733e1e37594e3e727fcadfb46c9abba4ba
Aleš Melichar <alesmelichar13@gmail.com>
2023-03-01 16:02:43 +0100

    Finished

--- main.py
+++ main.py
@@ -18,9 +18,10 @@ def setup_camera():
 
 
 cam = setup_camera()
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-ROWS = 6
-COLS = 8
+ROWS = 5
+COLS = 7
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((COLS * ROWS, 3), np.float32)
@@ -30,51 +31,49 @@ objp[:, :2] = np.mgrid[0:COLS, 0:ROWS].T.reshape(-1, 2)
 objpoints = []  # 3d point in real world space
 imgpoints = []  # 2d points in image plane.
 images = glob.glob('*.jpg')
-criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-i = 0
 
-# while True:
-#     img = process_image(WIDTH, HEIGHT)
-#     cv.imshow("img", img)
-#     pressed = cv.waitKey(1)
-#     if pressed == ord(' '):
-#         cv.imwrite("chessboard{0}.jpg".format(i), img)
-#         i += 1
-#     elif pressed == ord('q'):
-#         exit(0)
-images = glob.glob('*.jpg')
+images = glob.glob('*.jpg') # read all images
 
 for fname in images:
     img = cv.imread(fname)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     # Find the chess board corners
-    ret, corners = cv.findChessboardCorners(gray, (7,6), None)
+    ret, corners = cv.findChessboardCorners(gray, (COLS, ROWS), None)
     # If found, add object points, image points (after refining them)
-    if ret == True:
+    if ret:
         objpoints.append(objp)
-        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+        corners2 = cv.cornerSubPix(gray,corners, (11, 11), (-1, -1), criteria)
         imgpoints.append(corners2)
         # Draw and display the corners
-        cv.drawChessboardCorners(img, (7,6), corners2, ret)
-        cv.imshow('img', img)
-        cv.waitKey(500)
+        cv.drawChessboardCorners(img, (COLS, ROWS), corners2, ret)
+        # cv.imshow('img', img)
+        # cv.waitKey(500)
 
-# while cv.waitKey(1) != ord('q'):
-#     img = process_image(WIDTH, HEIGHT)
-#     cv.imshow("img", img)
-#
-#     gray_image = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-#     # find corners
-#     ret, corners = cv.findChessboardCorners(gray_image, (COLS, ROWS), None)
-#     # If found, add object points, image points (after refining them)
-#     if ret == True:
-#         objpoints.append(objp)
-#         corners2 = cv.cornerSubPix(gray_image, corners, (11, 11), (-1, -1), criteria)
-#         imgpoints.append(corners2)
-#         # Draw and display the corners
-#         cv.drawChessboardCorners(img, (7, 6), corners2, ret)
-#         cv.imshow('img', img)
-#         cv.waitKey(500)
+
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+img = cv.imread('chessboard0.jpg')
+h,  w = img.shape[:2]
+newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+# undistort
+dst = cv.undistort(img, mtx, dist, None, newcameramtx)
+# crop the image
+x, y, w, h = roi
+dst = dst[y:y+h, x:x+w]
+cv.imwrite('calibresult.png', dst)
+# undistort
+mapx, mapy = cv.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w,h), 5)
+dst = cv.remap(img, mapx, mapy, cv.INTER_LINEAR)
+# crop the image
+x, y, w, h = roi
+dst = dst[y:y+h, x:x+w]
+cv.imwrite('calibresult2.png', dst)
+
+mean_error = 0
+for i in range(len(objpoints)):
+    imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+    error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
+    mean_error += error
+print( "total error: {}".format(mean_error/len(objpoints)) )
 
 cv.destroyAllWindows()
 
